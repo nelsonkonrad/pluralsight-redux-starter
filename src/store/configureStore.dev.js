c1e02a8c9b52df5eff05083c17a0f49a28379a33
Nelson Orduz <norduz@konradgroup.com>
2017-01-10 17:03:41 -0600

    Config Redux Extension

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,15 +1,16 @@
-import {createStore, applyMiddleware} from 'redux';                         // applyMiddleware: To add middleware
+import {createStore, applyMiddleware, compose} from 'redux';                         // applyMiddleware: To add middleware
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 
 // App entry point
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const logger = createLogger();
 export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk, reduxImmutableStateInvariant())
+        composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
     );
 }
